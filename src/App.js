4f2b7c188e5bd8345430f004af624ff53b3aca04
HimanshuMaheshwari <himanshu.maheshwari54@gmail.com>
2023-08-04 20:09:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="mt-5 ">
+      <div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Description</th>
+                <th>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>2</th>
+                <td>Java learing</td>
+                <td>youtube</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TodoRowItem.js
@@ -0,0 +1,16 @@
+function TodoRowItem(){
+  const rowNumber = 1;
+  const rowDescription = "learn react";
+  const rowAssigned = "Himanshu Maheshwari";
+
+  return(
+      <tr>
+        <th scope='row'>{rowNumber}</th>
+        <td>{rowDescription}</td>
+        <td>{rowAssigned}</td>
+    </tr>
+  )
+
+}
+
+export default TodoRowItem;
